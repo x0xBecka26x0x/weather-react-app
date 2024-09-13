ed3e58ad4ab46282dffc3068522e2b77f81e12a1
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-09-12 22:50:59 -0400

    trying to fix loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
+import { Loader } from "react-loader-spinner";
 
 export default function Weather(props) {
     function handleResponse(response) {
